8dae668b024f194e01f244d62e606329b2d35e35
Miguel Tang <A01610836@itesm.mx>
2022-09-02 18:31:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appatemporal/framework/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.appatemporal.framework.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appatemporal.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
