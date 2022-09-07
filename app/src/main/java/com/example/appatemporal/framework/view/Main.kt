6b65d67abc3f496bc9875c7b977d3271843e107d
MandukoGR <gurom84@outlook.com>
2022-09-07 09:00:52 -0500

    add projects page

--- app/src/main/java/com/example/appatemporal/framework/view/Main.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/Main.kt
@@ -2,12 +2,17 @@ package com.example.appatemporal.framework.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appatemporal.R
 
-class MainActivity : AppCompatActivity() {
+class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
+
+
+
 }
