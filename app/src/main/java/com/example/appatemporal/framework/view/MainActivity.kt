f06bfe237f664f9597560eb97558f7827f7e2381
CristianA01702752 <a01702752@tec.mx>
2022-09-07 20:39:09 -0500

    add few changes to modificar proyecto

--- app/src/main/java/com/example/appatemporal/framework/view/MainActivity.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.appatemporal.framework.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appatemporal.R
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
