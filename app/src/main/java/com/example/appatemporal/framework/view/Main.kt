3600265469a04499cf1f2a3be3b4897023015be2
MandukoGR <gurom84@outlook.com>
2022-09-07 21:23:11 -0500

    add hilt RoomModule

--- app/src/main/java/com/example/appatemporal/framework/view/Main.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/Main.kt
@@ -8,12 +8,17 @@ import com.example.appatemporal.R
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.EditText
+import com.example.appatemporal.data.localdatabase.dao.ActividadDao
+import com.example.appatemporal.data.localdatabase.entities.Actividad
 
 class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Insert activity into database
+
 
 
 
