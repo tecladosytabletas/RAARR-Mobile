8afd8ab6f5d9426f7c4463b5149f85e3304121ca
FlavioRr <A01367631@itesm.mx>
2022-09-16 20:13:41 -0500

    Modificar actividad terminada

--- app/src/main/java/com/example/appatemporal/framework/view/ModificarActividad.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/ModificarActividad.kt
@@ -10,28 +10,30 @@ import com.example.appatemporal.domain.Repository
 import com.example.appatemporal.framework.viewModel.DeleteActivityViewModel
 import com.example.appatemporal.data.localdatabase.entities.Actividad
 import com.example.appatemporal.data.localdatabase.entities.Proyecto
+import com.example.appatemporal.databinding.ModifyNewActivityBinding
 import kotlinx.coroutines.launch
 
-class ModificarActividad : AppCompatActivity(){
-    private val  viewModel : DeleteActivityViewModel by viewModels()
+class ModificarActividad : AppCompatActivity() {
+    private val viewModel: DeleteActivityViewModel by viewModels()
     private lateinit var binding: ModifyNewActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ModifyNewActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val repository = Repository(this)
-        var myExtras :Bundle? = intent.extras
+        var myExtras: Bundle? = intent.extras
         binding.nameModifyActivity.setText(myExtras?.getString("nombre_actividad"))
-        val idactividad: Int = myExtras?.getInt("id_actividad")?:-1
+        val idactividad: Int = myExtras?.getInt("id_actividad") ?: -1
         // Set click listener
         binding.modifybutton.setOnClickListener {
             val name = binding.nameModifyActivity.text.toString()
             val actividad: Actividad = Actividad(idactividad, name)
-            lifecycleScope.launch{
+            lifecycleScope.launch {
                 viewModel.updateActividad(actividad, repository)
             }
             val intent = Intent(this, ProyectoOrganizador::class.java)
             startActivity(intent)
         }
 
+    }
 }
