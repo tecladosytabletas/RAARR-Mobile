6c77a5cb5457f081199f8d4a6d0997055d05ccae
halimabraham <a01769137@tec.mx>
2022-09-19 18:17:13 -0500

    added Contact Info view

---
+++ app/src/main/java/com/example/appatemporal/framework/view/ContactInfo.kt
@@ -0,0 +1,14 @@
+package com.example.appatemporal.framework.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appatemporal.databinding.ActivityContactInfoBinding
+
+class ContactInfo : AppCompatActivity() {
+    private lateinit var binding: ActivityContactInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityContactInfoBinding.inflate(layoutInflater)
+    }
+}
