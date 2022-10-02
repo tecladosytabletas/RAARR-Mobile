611c925790b8e6f2b1d663d940ce52b48f8f3efb
CristianA01702752 <a01702752@tec.mx>
2022-10-02 18:37:44 -0500

    Add random color function to cards in categories and changes to navbar_espectador.xml

--- app/src/main/java/com/example/appatemporal/framework/view/adapters/CategoriasEventosViewHolder.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/adapters/CategoriasEventosViewHolder.kt
@@ -1,21 +1,31 @@
 package com.example.appatemporal.framework.view.adapters
 
 import android.content.Intent
+import android.graphics.drawable.GradientDrawable
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.appatemporal.R
 import com.example.appatemporal.databinding.CategoriasEventosItemBinding
-import com.example.appatemporal.domain.Repository
 import com.example.appatemporal.domain.models.CategoryModel
-import com.example.appatemporal.framework.view.ActivityProyectoOrganizador
 import com.example.appatemporal.framework.view.EventosFiltrados
-import com.example.appatemporal.framework.viewModel.CategoriasEventosViewModel
-import com.example.appatemporal.framework.viewModel.ProyectoOrganizadorViewModel
+import kotlinx.android.synthetic.main.categorias_eventos_item.view.*
+import java.util.*
+
 
 class CategoriasEventosViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     private val binding = CategoriasEventosItemBinding.bind(view)
 
     fun bind(categoria: CategoryModel) {
+        with(itemView) {
+            val colors = resources.getIntArray(R.array.random_color)
+            val randomColor = colors[Random().nextInt(colors.size)]
+
+            val shape = GradientDrawable()
+            shape.cornerRadius = 18F
+            shape.setColor(randomColor)
+            tvCategoria.background = shape
+        }
         binding.tvCategoria.text = categoria.nombre
 
         binding.tvCategoria.setOnClickListener{
