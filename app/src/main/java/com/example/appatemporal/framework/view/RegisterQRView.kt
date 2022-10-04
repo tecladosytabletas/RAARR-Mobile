c9151092ad2d9a8fcdb73570c3ebc36c19f409eb
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2022-10-03 23:51:31 -0500

    fix scanQR function

--- app/src/main/java/com/example/appatemporal/framework/view/RegisterQRView.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/RegisterQRView.kt
@@ -64,15 +64,19 @@ class RegisterQRView : AppCompatActivity() {
             if (result.contents == null) {
                 Toast.makeText(this, "No se encontró un valor.", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Boleto escaneado con éxito.", Toast.LENGTH_SHORT).show()
-                scanQRViewModel.updateTicketValue(result.contents, repository)
-                scanQRViewModel.qrcode.observe(this, Observer {
-                    if(it == true){
-                        Toast.makeText(this,"Boleto valido!",Toast.LENGTH_SHORT).show()
-                    }else{
-                        Toast.makeText(this,"Boleto ya fue Escaneado.",Toast.LENGTH_SHORT).show()
+                scanQRViewModel.verifyTicketExistence(result.contents, repository) {
+                    if (it) {
+                        scanQRViewModel.updateTicketValue(result.contents, repository) {
+                            if(it){
+                                Toast.makeText(this,"Boleto valido escaneado EXITOSAMENTE",Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this,"Boleto YA USADO",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    } else {
+                        Toast.makeText(this,"Boleto INVALIDO",Toast.LENGTH_SHORT).show()
                     }
-                })
+                }
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
--- app/src/main/java/com/example/appatemporal/framework/viewModel/ScanQRViewModel.kt
+++ app/src/main/java/com/example/appatemporal/framework/viewModel/ScanQRViewModel.kt
@@ -5,18 +5,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.appatemporal.data.GetUserLocalDBRequirement
 import com.example.appatemporal.data.UpdateTicketValueRequirement
+import com.example.appatemporal.data.VerifyRatingExistanceRequirement
+import com.example.appatemporal.data.VerifyTicketExistenceRequirement
 import com.example.appatemporal.data.localdatabase.entities.Usuario
 import com.example.appatemporal.domain.Repository
 import kotlinx.coroutines.launch
 
 class ScanQRViewModel : ViewModel() {
-    val qrcode = MutableLiveData<Boolean>()
+    //val qrcode = MutableLiveData<Boolean>()
     private val updateTicketValueRequirement = UpdateTicketValueRequirement()
+    private val verifyTicketExistenceRequirement = VerifyTicketExistenceRequirement()
 
-    fun updateTicketValue(resulted: String, repository:Repository) {
+    fun verifyTicketExistence(resulted: String, repository: Repository, result: (Boolean) -> Unit) {
+        viewModelScope.launch {
+            val existence = verifyTicketExistenceRequirement(resulted, repository)
+            result(existence)
+        }
+    }
+
+    fun updateTicketValue(resulted: String, repository:Repository, result: (Boolean) -> Unit) {
         viewModelScope.launch {
             val qrHash = updateTicketValueRequirement(resulted, repository)
-            qrcode.postValue(qrHash)
+            result(qrHash)
+            //qrcode.postValue(qrHash)
         }
     }
 }
