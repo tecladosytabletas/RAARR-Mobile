c0eaad6dcdf34c1fe2917da606e884188fc01e48
Andrés Magaña Pérez <a01067963@itesm.mx>
2022-10-05 11:14:43 -0500

    Homepage Organizador Recyclers Views

--- app/src/main/java/com/example/appatemporal/framework/view/ActivityMainHomepageOrganizador.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/ActivityMainHomepageOrganizador.kt
@@ -9,22 +9,27 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.appatemporal.databinding.ActivityMainHomepageOrganizadorBinding
 import com.example.appatemporal.domain.Repository
+import com.example.appatemporal.framework.view.adapters.ActivityMainHomepageOrganizadorAdapterHorizontal
+import com.example.appatemporal.framework.view.adapters.ActivityMainHomepageOrganizadorAdapterVertical
 import com.example.appatemporal.framework.view.adapters.boletosPorEventoAdapter
 import com.example.appatemporal.framework.viewModel.GetEventsInMonthViewModel
+import com.example.appatemporal.framework.viewModel.GetEventsUserOrg
 import com.example.appatemporal.framework.viewModel.GetUserTicketViewModel
 
 class ActivityMainHomepageOrganizador : AppCompatActivity() {
     private lateinit var binding: ActivityMainHomepageOrganizadorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val getEventsInMonthViewModel : GetEventsInMonthViewModel by viewModels()
+        val getEventsUserOrg : GetEventsUserOrg by viewModels()
         val repository = Repository(this)
         super.onCreate(savedInstanceState)
         binding = ActivityMainHomepageOrganizadorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val userIdTemp = "pod6xLDUeRNZItm7u93DC5CYbgJ2"
-        //initRecyclerViewHorizontal(getEventsInMonthViewModel, userIdTemp, repository)
+
+        initRecyclerViewHorizontal(getEventsUserOrg, userIdTemp, repository)
+        initRecyclerViewVertical(getEventsUserOrg, repository)
 
         binding.navbar.homeIcon.setOnClickListener {
             finish()
@@ -46,21 +51,25 @@ class ActivityMainHomepageOrganizador : AppCompatActivity() {
 
     }
 
-    private fun initRecyclerViewHorizontal(getTicketViewModel: GetUserTicketViewModel, userIdTemp: String, repository: Repository){
-        getTicketViewModel.getUserTicket(userIdTemp, repository)
-        Log.d("LOG Activity",getTicketViewModel.getUserTicket(userIdTemp, repository).toString())
-        getTicketViewModel.ticket.observe(this, Observer {
-            binding.HorizontalOrgView.layoutManager = LinearLayoutManager(this) // Le da el layout que usará el RV.
-            binding.HorizontalOrgView.adapter = boletosPorEventoAdapter(it)
+    private fun initRecyclerViewHorizontal(getEventsUserOrg: GetEventsUserOrg, userIdTemp: String, repository: Repository){
+        getEventsUserOrg.getEventsOrg(userIdTemp, repository)
+        // Log.d("LOG Activity",getEventsUserOrg.getEventsOrg(userIdTemp, repository).toString())
+        getEventsUserOrg.allEventsOrg.observe(this, Observer { eventList ->
+            val linearLayout = LinearLayoutManager(this)
+            linearLayout.orientation=LinearLayoutManager.HORIZONTAL
+            binding.HorizontalOrgView.layoutManager = linearLayout // Le da el layout que usará el RV.
+            binding.HorizontalOrgView.adapter = ActivityMainHomepageOrganizadorAdapterHorizontal(eventList)
         })
     }
 
-    private fun initRecyclerViewVertical(getTicketViewModel: GetUserTicketViewModel, userIdTemp: String, repository: Repository){
-        getTicketViewModel.getUserTicket(userIdTemp, repository)
-        Log.d("LOG Activity",getTicketViewModel.getUserTicket(userIdTemp, repository).toString())
-        getTicketViewModel.ticket.observe(this, Observer {
-            binding.HorizontalOrgView.layoutManager = LinearLayoutManager(this) // Le da el layout que usará el RV.
-            binding.HorizontalOrgView.adapter = boletosPorEventoAdapter(it)
+    private fun initRecyclerViewVertical(getEventsUserOrg: GetEventsUserOrg, repository: Repository){
+        getEventsUserOrg.getAllEvents(repository)
+        // Log.d("LOG Activity",getEventsInMonthViewModel.getAllEvents(repository).toString())
+        getEventsUserOrg.allEvents.observe(this, Observer { eventList ->
+            val linearLayout = LinearLayoutManager(this)
+            linearLayout.orientation=LinearLayoutManager.VERTICAL
+            binding.HorizontalOrgView.layoutManager = linearLayout // Le da el layout que usará el RV.
+            binding.HorizontalOrgView.adapter = ActivityMainHomepageOrganizadorAdapterVertical(eventList)
         })
     }
 
--- app/src/main/java/com/example/appatemporal/framework/view/adapters/ActivityMainHomepageOrganizadorAdapterVertical.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/adapters/ActivityMainHomepageOrganizadorAdapterVertical.kt
@@ -8,13 +8,13 @@ import com.example.appatemporal.R
 import com.example.appatemporal.domain.models.EventModel
 import com.example.appatemporal.domain.models.EventsInMonth
 
-class ActivityMainHomepageOrganizadorAdapterVertical(private val list: MutableList<EventModel>) : RecyclerView.Adapter<ActivityMainHomepageEspectadorViewHolderVertical>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityMainHomepageEspectadorViewHolderVertical {
+class ActivityMainHomepageOrganizadorAdapterVertical(private val list: MutableList<EventModel>) : RecyclerView.Adapter<ActivityMainHomepageOrganizadorViewHolderVertical>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityMainHomepageOrganizadorViewHolderVertical {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return ActivityMainHomepageEspectadorViewHolderVertical(layoutInflater.inflate(R.layout.activity_homepage_tarjeta_eventos_grande_espectador, parent, false))
+        return ActivityMainHomepageOrganizadorViewHolderVertical(layoutInflater.inflate(R.layout.activity_homepage_tarjeta_eventos_grande_espectador, parent, false))
     }
 
-    override fun onBindViewHolder(holder: ActivityMainHomepageEspectadorViewHolderVertical, position: Int) {
+    override fun onBindViewHolder(holder: ActivityMainHomepageOrganizadorViewHolderVertical, position: Int) {
         val item = list[position]
         Log.d("listLog", list.toString())
         holder.render(item)
