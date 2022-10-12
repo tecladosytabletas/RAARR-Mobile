2545432d2f2f433672604a6c273a8776adf58a24
FlavioRr <A01367631@itesm.mx>
2022-10-12 17:43:54 -0500

    JavaDoc Documentation on Costos and Modify Activity

--- app/src/main/java/com/example/appatemporal/framework/view/AddCosto.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/AddCosto.kt
@@ -3,7 +3,11 @@ package com.example.appatemporal.framework.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appatemporal.databinding.ActivityProyectoOrganizadorBinding
-
+/**
+ * This file is linked with add_costos.xml
+ * This file is in charge of controlling the logic behind the funtion add a costo
+ *
+ * */
 class AddCosto : AppCompatActivity() {
     private lateinit var binding: ActivityProyectoOrganizadorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
