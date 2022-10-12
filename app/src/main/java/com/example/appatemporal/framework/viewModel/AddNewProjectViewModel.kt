06d76b212b5fe2c63e533d317415d9dae1cc82e0
AriannFernando <A01703556@itesm.mx>
2022-10-12 13:08:47 -0500

    add comments to projects and presupuesto to requirements, viewModels

--- app/src/main/java/com/example/appatemporal/framework/viewModel/AddNewProjectViewModel.kt
+++ app/src/main/java/com/example/appatemporal/framework/viewModel/AddNewProjectViewModel.kt
@@ -7,7 +7,14 @@ import com.example.appatemporal.domain.Repository
 
 
 class AddNewProjectViewModel: ViewModel() {
+    // variable to access the add project requirement docs
     private val requirement = AddNewProjectRequirement()
+
+    /*
+    * Function to create an project item in the local database
+    * @param proyecto - The entity called Proyecto
+    * @param repository - The context to access the resources assigned to the local database
+    */
     suspend fun addNewProject(proyecto: Proyecto, repository: Repository){
 
         requirement.createProject(proyecto, repository)
--- app/src/main/java/com/example/appatemporal/framework/viewModel/PresupuestoOrganizadorViewModel.kt
+++ app/src/main/java/com/example/appatemporal/framework/viewModel/PresupuestoOrganizadorViewModel.kt
@@ -9,9 +9,17 @@ import com.example.appatemporal.domain.Repository
 import kotlinx.coroutines.launch
 
 class PresupuestoOrganizadorViewModel: ViewModel()  {
-
+        // variable to access the add project requirement docs
         private val requirement = PresupuestoOrganizadorRequirement()
+        // variable that is a live data project
         val project = MutableLiveData<Proyecto>()
+
+        /*
+        * Function to update presupuesto of a project and establish the project again
+        * @param presupuestoNew - The String that is the new presupuesto of the project
+        * @param id - The Int that is the id of the project that will be updated
+        * @param repository - The context to access the resources assigned to the local database
+        */
         fun updatePrespuesto(presupuestoNew: Double, id: Int, repository: Repository){
             viewModelScope.launch {
                 requirement.updatePresupuestos(presupuestoNew, id, repository)
@@ -19,6 +27,13 @@ class PresupuestoOrganizadorViewModel: ViewModel()  {
                 project.postValue(proyecto)
             }
         }
+
+        /*
+        * Function to get the project that a project has according to the local database according
+        * to it´s id and establish the project again
+        * @param id - The id of the project the user selected
+        * @param repository - The context to access the resources assigned to the local database
+        */
         fun getProyectoByid(id: Int, repository: Repository){
             viewModelScope.launch {
                 val proyecto = requirement.getProyectoById(id, repository)
@@ -27,6 +42,12 @@ class PresupuestoOrganizadorViewModel: ViewModel()  {
             }
         }
 
+        /*
+        * Function to update meta of a project and establish the project again
+        * @param metaNew - The Double that is the new meta of the project
+        * @param id - The Int that is the id of the project that will be updated
+        * @param repository - The context to access the resources assigned to the local database
+        */
         fun updateMeta(metaNew: Double, id: Int, repository: Repository){
             viewModelScope.launch {
                 requirement.updateMeta(metaNew, id, repository)
