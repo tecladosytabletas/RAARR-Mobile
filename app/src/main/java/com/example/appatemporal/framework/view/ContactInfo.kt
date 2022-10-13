8d772c21b2b98bb4476757e34697839b8a12d1a6
AlejandroRuizZ <robinthemage1@gmail.com>
2022-10-12 19:37:28 -0500

    Java Doc Documentation of ContactInfo Functionality.

--- app/src/main/java/com/example/appatemporal/framework/view/ContactInfo.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/ContactInfo.kt
@@ -4,10 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appatemporal.databinding.ActivityContactInfoBinding
-
+/**
+ * Class that inherits from AppCompactActivity, in charge of Displaying ContactInfo
+ *
+ */
 class ContactInfo : AppCompatActivity() {
     private lateinit var binding: ActivityContactInfoBinding
-
+    /**
+     * Overrides function onCreate and starts the activity
+     *
+     * @param savedInstanceState: Bundle? -> Saved instance of the activity
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityContactInfoBinding.inflate(layoutInflater)
