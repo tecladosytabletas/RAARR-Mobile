68bc8adccacc7b9b1e0cfd9bd97bb10b470e20b6
Armando Gutiérrez <75638601+MandukoGR@users.noreply.github.com>
2022-10-12 19:46:58 -0500

    Update ProyectoOrganizadorViewModel.kt

--- app/src/main/java/com/example/appatemporal/framework/viewModel/ProyectoOrganizadorViewModel.kt
+++ app/src/main/java/com/example/appatemporal/framework/viewModel/ProyectoOrganizadorViewModel.kt
@@ -17,7 +17,7 @@ class ProyectoOrganizadorViewModel: ViewModel() {
     // variable that is a live data project
     val project = MutableLiveData<Proyecto>()
 
-    /*
+    /**
     * Function to delete a project and establish the list projects again
     * @param proyecto - a variable that represents that will extend the entity called Proyecto
     * @param repository - The context to access the resources assigned to the local database
@@ -30,7 +30,7 @@ class ProyectoOrganizadorViewModel: ViewModel() {
         }
     }
 
-    /*
+    /**
     * Function to update a project
     * @param name - The String that is the name of the project
     * @param date - The String that is the date of the project
@@ -45,7 +45,7 @@ class ProyectoOrganizadorViewModel: ViewModel() {
 
     }
 
-    /*
+    /**
     * Function to get all projects according to the local database and establish the list projects again
     * @param repository - The context to access the resources assigned to the local database
     * @return - A list containing all projects
@@ -57,7 +57,7 @@ class ProyectoOrganizadorViewModel: ViewModel() {
         }
     }
 
-    /* Function that we use to count all the activities with a certain status attribute registered on a project
+    /** Function that we use to count all the activities with a certain status attribute registered on a project
     * @param repository - The context to access the resources assigned to the local database
     * @param id - The id of the project the user selected
     * @param stringStatus - The string that it will use to search in activities
@@ -67,7 +67,7 @@ class ProyectoOrganizadorViewModel: ViewModel() {
         return requirement.countPendingActivities(repository, id_a, stringStatus)
     }
 
-    /* Function that we use to count all the activities with a certain status attribute registered on a project
+    /** Function that we use to count all the activities with a certain status attribute registered on a project
     * @param repository - The context to access the resources assigned to the local database
     * @param id - The id of the project the user selected
     * @param stringStatus - The string that it will use to search in activities
@@ -76,7 +76,7 @@ class ProyectoOrganizadorViewModel: ViewModel() {
     fun countDoneActivities(repository: Repository, id_a: Int, stringStatus: String): Int{
         return requirement.countDoneActivities(repository, id_a, stringStatus)
     }
-    /* Function that we use to count all the activities registered on a project
+    /** Function that we use to count all the activities registered on a project
     * @param repository - The context to access the resources assigned to the local database
     * @param id - The id of the project the user selected
     * @return The number of all the activities found in the local database
@@ -85,7 +85,7 @@ class ProyectoOrganizadorViewModel: ViewModel() {
         return requirement.countAllActivities(repository, id_a)
     }
 
-    /*
+    /**
     * Function used to extract all the projects that has a specific status
     * @param stringStatus - The boolean that will be used to search in the local database
     * @param repository - The context to access the resources assigned to the local database
